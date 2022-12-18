4a2a60c55f58eb66f04d686415eb71f9a8ab51ff
CRBroughton <crbroughton@posteo.uk>
2022-12-18 20:39:03 +0000

    init commit

---
+++ src/components/weather.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="weather-container">
+    <div class="weather-location">
+      <p>Brighton</p>
+      <a href="https://accuweather.com">Accuweather</a>
+    </div>
+    <div class="weather-information">
+      <img src="http://openweathermap.org/img/wn/10d@4x.png" alt="Weather icon">
+      <div class="weather-nested-information">
+        <div class="weather-warning">
+          <p class="weather-temperature">
+            3°
+          </p>
+          <div class="weather-warning-text">
+            High Temperature
+          </div>
+        </div>
+        <p>Thundershower | Lightly Polluted</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+p {
+  margin: 0;
+  padding: 0;
+}
+.weather-warning-text {
+  color: white;
+  display: flex;
+  background: rgb(255, 102, 0);
+  height: 2em;
+  padding: 0.2em 0.6em;
+  align-items: center;
+  border-radius: 15px;
+}
+.weather-warning {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.weather-nested-information {
+  display: flex;
+  flex-direction: column;
+}
+.weather-information {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+img {
+  width: 120px;
+  height: 120px;
+}
+.weather-location {
+  display: flex;
+  place-items: center;
+  justify-content: space-between;
+}
+.weather-container {
+    padding: 1em 1.5em;
+    background-color: hsl(0, 10%, 98%);
+    max-width: 24em;
+    max-height: 20em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    border-radius: 15px;
+    /* border: 1px solid rgba(99, 99, 99, 0.2); */
+    box-shadow: rgba(99, 99, 99, 0.2) 5px 2px 8px 0px;}
+
+.weather-temperature {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 3em;
+    font-weight: 500;
+}
+</style>
---
+++ src/stories/weather.story.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import weather from '../components/weather.vue'
+</script>
+
+<template>
+  <Story>
+    <weather />
+  </Story>
+</template>
+
+<style scoped>
+
+</style>
