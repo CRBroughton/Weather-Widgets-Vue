34171be5bb425bdbeee7636f5b12fccbae96435c
CRBroughton <crbroughton@posteo.uk>
2023-01-04 19:39:49 +0000

    feat: :sparkles: add api client

---
+++ src/api/client.ts
@@ -0,0 +1,18 @@
+import type { OpenWeatherResponse } from './types'
+import { httpClient } from './wrapper'
+
+interface Params {
+  apikey: string
+  lat: string
+  lon: string
+}
+
+export class ApiClient {
+  async fetchWeatherData(params: Params) {
+    return httpClient.get<OpenWeatherResponse>(
+      {
+        url: `https://api.openweathermap.org/data/3.0/onecall?lat=${params.lat}8&lon=${params.lon}&units=metric&appid=${params.apikey}`,
+      },
+    )
+  }
+}
