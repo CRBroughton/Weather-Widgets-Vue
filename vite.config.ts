590f6e1fe7596930c130db61059b2ae8021dbb17
CRBroughton <crbroughton@posteo.uk>
2023-01-09 20:22:37 +0000

    build: :building_construction: add build config to vite and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'weather-widgets-vue',
+      fileName: format => `weather-widgets-vue-${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
